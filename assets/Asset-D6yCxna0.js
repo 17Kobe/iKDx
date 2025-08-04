5a6f6f5749986b4254cfc512b60b475ea0b98633
17Kobe <17Kobe@users.noreply.github.com>
2025-08-04 07:11:48 +0000

    deploy: 604a70f87adb739e288e31867c79f50deec9383d

---
+++ assets/Asset-D6yCxna0.js
@@ -0,0 +1,1 @@
+import{_ as e,S as t,O as s}from"./index-CJv5UaZ9.js";const c={},n={style:{padding:"32px"}};function o(r,_){return s(),t("div",n,"資產表頁面")}const d=e(c,[["render",o]]);export{d as default};
