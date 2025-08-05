4754661c15979c7e2c960081ef056cc1af9d8242
17Kobe <17Kobe@users.noreply.github.com>
2025-08-05 12:59:17 +0000

    deploy: b33db39b13619d521b64717b620994fd23c79d02

---
+++ assets/Asset-CnQhBQbi.js
@@ -0,0 +1,1 @@
+import{ac as e,a3 as t,$ as c}from"./index-Q1UO9hM0.js";const s={},n={style:{padding:"32px"}};function o(a,r){return c(),t("div",n,"資產表頁面")}const d=e(s,[["render",o]]);export{d as default};
