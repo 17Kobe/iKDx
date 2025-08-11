b9bb1b4ad00481be49b6c86fd697d35464dedc3f
17Kobe <17Kobe@users.noreply.github.com>
2025-08-11 06:11:24 +0000

    deploy: 7baab8386b029c3185d779810af39d2148e17d4c

---
+++ assets/Asset-D0Iugl01.js
@@ -0,0 +1,1 @@
+import{aA as e,at as t,ap as s}from"./index-5xjI7_EN.js";const c={},a={style:{padding:"32px"}};function n(o,r){return s(),t("div",a,"資產表頁面")}const d=e(c,[["render",n]]);export{d as default};
