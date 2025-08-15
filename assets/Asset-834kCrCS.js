bdff2d2d4c1cd049d102e999365fe18d1706e4f2
17Kobe <17Kobe@users.noreply.github.com>
2025-08-15 10:38:00 +0000

    deploy: aa8b5fb1608e08e231d721a097fce2f5f382a724

---
+++ assets/Asset-834kCrCS.js
@@ -0,0 +1,1 @@
+import{az as e,as as s,ao as t}from"./index-BQeGbiwi.js";const c={},o={style:{padding:"32px"}};function a(n,r){return t(),s("div",o,"資產表頁面")}const d=e(c,[["render",a]]);export{d as default};
